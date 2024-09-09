041785be48fcc08874bcd804ee81703de21ea2f8
Victor <victormwendwa804@gmail.com>
2024-09-09 12:28:47 +0300

    Adding navigation using css

--- THE24_Website/THE24_Website/settings.py
+++ THE24_Website/THE24_Website/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    "blog.apps.BlogConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",

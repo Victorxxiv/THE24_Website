ee6e8c6d195079459df8c5c20b706f929f01bb65
Victor <victormwendwa804@gmail.com>
2024-09-09 11:26:29 +0300

    rendering html templates for blog

--- THE24_Website/blog/views.py
+++ THE24_Website/blog/views.py
@@ -1,8 +1,25 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+posts = [
+    {
+        'author': 'Victor',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'September 09, 2024'
+    },
+    {
+        'author': 'Ragnar',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 10, 2024'
+    }
+]
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})

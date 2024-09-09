545072ff9a65deadf236914a87bb69b6a64a1c7e
Victor <victormwendwa804@gmail.com>
2024-09-09 09:14:50 +0300

    Setting up my project

---
+++ THE24_Website/THE24_Website/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for THE24_Website project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "THE24_Website.settings")
+
+application = get_asgi_application()

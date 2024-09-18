4cf29e7a26db4d155f67c2f8f58fa0221bfe9988
Victor <victormwendwa804@gmail.com>
2024-09-18 05:33:08 +0300

    Data creation using postgresql

--- THE24_Website/blog/views.py
+++ THE24_Website/blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Victor',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'September 09, 2024'
-    },
-    {
-        'author': 'Ragnar',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'September 10, 2024'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 

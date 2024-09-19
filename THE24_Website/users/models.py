cb25db08fd0f25692ec3f6d0af338cb873050b24
Victor <victormwendwa804@gmail.com>
2024-09-19 05:06:40 +0300

    Creating a User Profile Model

--- THE24_Website/users/models.py
+++ THE24_Website/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create profile model
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    bio = models.TextField(blank=True, null=True)  # Allows bio to be empty
+    city = models.CharField(max_length=100, blank=True, null=True)  # City name
+
+    def __str__(self):
+        return f'{self.user.username} Profile'

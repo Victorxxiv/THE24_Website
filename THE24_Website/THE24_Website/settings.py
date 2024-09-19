b82cd740eb8f370a05ff7886bda10909ea23c27d
Victor <victormwendwa804@gmail.com>
2024-09-19 05:28:12 +0300

    Creating media Base

--- THE24_Website/THE24_Website/settings.py
+++ THE24_Website/THE24_Website/settings.py
@@ -134,6 +134,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'

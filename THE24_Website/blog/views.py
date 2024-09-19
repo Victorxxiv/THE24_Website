1351ea94d9e955cc0ad26c1daa25610b19c6ca70
Victor <victormwendwa804@gmail.com>
2024-09-19 11:10:16 +0300

    dealing with listview and ordering

--- THE24_Website/blog/views.py
+++ THE24_Website/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from .models import Post
 
 def home(request):
@@ -6,6 +7,11 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})

a13a8cd9eeae9c944f5d64c6d0ab4ce1214eb0fd
Victor <victormwendwa804@gmail.com>
2024-09-20 08:51:12 +0300

    Deploying the update button for post

--- THE24_Website/blog/views.py
+++ THE24_Website/blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import (
     ListView,
     DetailView,
-    CreateView
+    CreateView,
+    UpdateView
 )
 from .models import Post
 
@@ -21,7 +23,15 @@ class PostListView(ListView):
 class PostDetailView(DetailView):
     model = Post
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 

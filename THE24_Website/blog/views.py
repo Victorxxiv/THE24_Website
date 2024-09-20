34e7b2d60f78d675222d14bc307c12c8346a88ff
Victor <victormwendwa804@gmail.com>
2024-09-20 05:57:44 +0300

    Implementing create view

--- THE24_Website/blog/views.py
+++ THE24_Website/blog/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView
+)
 from .models import Post
 
 def home(request):
@@ -13,6 +17,17 @@ class PostListView(ListView):
     template_name = 'blog/home.html' # <app>/<model>_<viewtype>.html
     context_object_name = 'posts'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
+
+class PostCreateView(CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})

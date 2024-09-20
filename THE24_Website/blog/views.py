9a44d945f37caad003fd3f5a89ef4391d07ce629
Victor <victormwendwa804@gmail.com>
2024-09-20 11:39:23 +0300

    using deleteview, for users to delete posts

--- THE24_Website/blog/views.py
+++ THE24_Website/blog/views.py
@@ -4,7 +4,8 @@ from django.views.generic import (
     ListView,
     DetailView,
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
 )
 from .models import Post
 
@@ -39,6 +40,15 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
     
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+
     def test_func(self):
         post = self.get_object()
         if self.request.user == post.author:

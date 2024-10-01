761864a34ea7ab1c2bcfa6ae536fdb1e74bd1fa9
Victor <victormwendwa804@gmail.com>
2024-10-01 14:45:00 +0300

    Preparing for deployment

--- THE24_Website/THE24_Website/settings.py
+++ THE24_Website/THE24_Website/settings.py
@@ -149,6 +149,7 @@ EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 

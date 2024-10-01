638c9d4436e4230696156f1bc08f4b244e25c7c7
Victor <victormwendwa804@gmail.com>
2024-10-01 13:05:38 +0300

    Setting up Email recovery

--- THE24_Website/THE24_Website/settings.py
+++ THE24_Website/THE24_Website/settings.py
@@ -142,6 +142,13 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 

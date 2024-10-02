e3538f6d88ecf77f3b2a6feb6b11820c9e87f7d3
Victor <victormwendwa804@gmail.com>
2024-10-02 18:34:56 +0300

    Setting Static files

--- THE24_Website/THE24_Website/settings.py
+++ THE24_Website/THE24_Website/settings.py
@@ -160,8 +160,9 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 

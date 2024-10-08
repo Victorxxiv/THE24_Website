04c8f5367b24037e5bb5b8e87cd85b09c07b2927
Victor <victormwendwa804@gmail.com>
2024-10-08 08:27:03 +0300

    Creating The home views

--- THE24_Website/home/views.py
+++ THE24_Website/home/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from blog.models import Post
 
-# Create your views here.
+def home(request):
+    latest_posts = Post.objects.all().order_by('-date_posted')[:5] # Fetch latest posts for sidebar or section
+    context = {
+        'latest_posts': latest_posts  # Pass latest posts to the home template
+    }
+    return render(request, 'home/home.html', context)

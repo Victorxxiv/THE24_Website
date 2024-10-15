9f3a0146517d60b0525c5f565df1cae2609b6dbd
Victor <victormwendwa804@gmail.com>
2024-10-15 08:38:03 +0300

    Staticfiles configration

--- THE24_Website/settings.py
+++ THE24_Website/settings.py
@@ -161,7 +161,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Enable ManifestStaticFilesStorage for static file versioning
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
